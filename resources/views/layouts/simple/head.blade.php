0c32f80d6c8c8dd7fac03a2da9c56715f35f774f
Alexis DJIDONOU <alexisdjidonou@gmail.com>
2025-09-30 18:49:37 +0000

    modification du header

--- resources/views/layouts/simple/head.blade.php
+++ resources/views/layouts/simple/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="keywords" content="ecommerce, store, mart, fashion, grocery, super store, market, organic food, super market">
+<meta name="keywords" content="Site de vente en ligne, de produit de beauté , parfums, cable de chargeur, ampoule connecté, tous ce dont vous utilisé pour le quotidian">
 <meta name="description" content="Site de vente en ligne, de produit de beauté , parfums, cable de chargeur, ampoule connecté, tous ce dont vous utilisé pour le quotidian">
 <meta name="author" content="boutik 17">
 <meta name="csrf-token" content="{{ csrf_token() }}">
